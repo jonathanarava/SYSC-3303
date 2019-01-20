e9ee8d084c06f605c02221981021e8d232ab02c7
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2019-01-19 19:40:42 -0500

    Update Snippets.java

--- Assignment 1/assignment1/src/assignment1/Snippets.java
+++ Assignment 1/assignment1/src/assignment1/Snippets.java
@@ -85,8 +85,8 @@ public void sendAndReceive(byte msg[]) {
 	   int len = sendPacket.getLength();
 	   System.out.println("Length: " + len);
 	   System.out.println("Containing: ");
-	   System.out.println("In string: ");
-	   System.out.println(new String(sendPacket.getData(),0,len)); // or could print "s"
+	   System.out.println("In string: " + new String(sendPacket.getData(),0,len));
+	   //System.out.println(new String(sendPacket.getData(),0,len)); // or could print "s"
 	   System.out.println("In byte array: ");
 	   System.out.println(Arrays.toString(msg));
 	   
@@ -130,8 +130,8 @@ public void sendAndReceive(byte msg[]) {
 		   System.out.println("Invalid Request");
 		   System.exit(1);
 	   }
-	   System.out.print("Received packet containing: \n" + "In byte array: \n");
-	   System.out.println(Arrays.toString(data));
+	   System.out.print("Received packet containing: \n" + "In byte array: \n" + Arrays.toString(data));
+	  // System.out.println(Arrays.toString(data));
 	   System.out.println("-------------------------------------------");
 	   // We're finished, so close the socket.
 	   //sendReceiveSocket.close();
